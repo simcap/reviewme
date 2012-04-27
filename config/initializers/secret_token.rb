669737b324981a834209c0fa6c3ffb13de83ef86
simcap <caplette@hotmail.co.uk>
2012-04-27 15:51:36 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Reviewme::Application.config.secret_token = 'b41610fada086047f023fc9565824410b332948f88516da503126b66463baf96b65f4921503584dcd93e9ef8759eb9a9b2fda8f374a6083ebcb70d070bd89372'
