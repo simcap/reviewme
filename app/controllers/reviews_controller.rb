2b0b5bca49b248911743df6e342881152ea9b24e
simcap <caplette@hotmail.co.uk>
2012-05-04 23:16:01 +0200

    sanitizing in put from command line on server

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,5 @@
 class ReviewsController < ApplicationController
+  before_filter :sanitize_review_attrs, :only => [:create]
   
   def index
     @reviews = Review.order("created_at ASC")
@@ -34,13 +35,27 @@ class ReviewsController < ApplicationController
     @review = Review.new_pending
     @review.attributes = params[:review]    
     @review.add_commits(params[:commit])
-    
+        
     if @review.save
       Reviewer.register_new_publisher_as_reviewer(@review)    
       render :text => "You have successfully published your commit for review to " + reviews_url + "... It'd better be clean and tested ;)"
     else
       render :text => "Could not save your review. Errors are: " << @review.errors.to_a.join(", ")
     end
+  end
+  
+  private
+  
+  # Allow to remove carriage returns and new lines from reviews submitted from git the command line
+  def sanitize_review_attrs
+    params[:review].each_value { |review_attr|
+      review_attr.chomp! unless review_attr.nil?
+    }
+    params[:commit].each { |commit|
+      commit.each_value { |commit_attr|
+        commit_attr.chomp! unless commit_attr.nil?        
+      }
+    }
   end
 
 end
